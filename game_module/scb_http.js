dac98d19682b46d647351fee797755414b607d37
Galbarad <steelprogram@gmail.com>
2019-03-18 09:27:55 +0200

    few fixes

--- game_module/scb_http.js
+++ game_module/scb_http.js
@@ -1,5 +1,7 @@
-const WARLOCK_LOGIN    = process.argv[2];
-const WARLOCK_PASSWORD = process.argv[3];
+var ARR_WARLOCK_LOGIN = process.argv[2].split(',');
+var WARLOCK_IDX       = 0;
+var WARLOCK_LOGIN     = ARR_WARLOCK_LOGIN[WARLOCK_IDX];
+var WARLOCK_PASSWORD  = process.argv[3];
 
 const SITE_LOGIN_URL = 'https://games.ravenblack.net/login';
 const SITE_STATUS_URL = 'https://games.ravenblack.net/player';
@@ -11,10 +13,10 @@ const SITE_WARLOCK_SUBMIT = 'https://games.ravenblack.net/warlocksubmit';
 var request = require('request');
 var fs = require('fs');
 
-const CHALLENGE_ADD_CONDITION = 3;
-const CHALLENGE_ACCEPT_CONDITION = 3;
+const CHALLENGE_ADD_CONDITION = 2;
+const CHALLENGE_ACCEPT_CONDITION = 5;
 
-const CYCLE_TIMEMOUT = 10 * 60 * 1000; // 10 minutes
+const CYCLE_TIMEMOUT = 20 * 1000;// 20 sec * 3 
 
 var warlock_logined = false;
 var fatal_error = false;
@@ -22,7 +24,9 @@ var battles_in_process = [];
 var battles_in_ready = [];
 var battles_in_wait = [];
 
+var idx = 0;
 function Cycle() {
+  console.log('Cycle ' + (++idx));
   if (warlock_logined) {
     console.log('logined = true');
     checkActiveBattle();
@@ -36,8 +40,17 @@ function Cycle() {
   }*/
 }
 
-function nextCycle() {
-  setTimeout(Cycle, CYCLE_TIMEMOUT);
+function nextCycle(force) {
+  console.log('nextCycle', force, CYCLE_TIMEMOUT);
+  if (!force) {// change login
+    if (++WARLOCK_IDX >= ARR_WARLOCK_LOGIN.length) {
+      WARLOCK_IDX = 0;
+    }
+    WARLOCK_LOGIN = ARR_WARLOCK_LOGIN[WARLOCK_IDX];
+    warlock_logined = false;
+    console.log('Change login', WARLOCK_LOGIN);
+  }
+  setTimeout(Cycle, force ? 1000 : CYCLE_TIMEMOUT);
 }
 
 function login() {
@@ -53,20 +66,24 @@ function login() {
      }, function(err,httpResponse,body) {
         if (err) {
           console.log('error:', err); // Print the error if one occurred
+          nextCycle();
           return;
         }
-        console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        console.log('login', 'statusCode:', httpResponse.statusCode, httpResponse.headers);
+        //console.log('body:', body); // Print the HTML for the Google homepage.
         if ((httpResponse.statusCode === 302) && (httpResponse.headers['location'] === 'player')) {
           warlock_logined = true;
-          nextCycle();
-        } else {
+          nextCycle(true);
+        } else if (httpResponse.statusCode === 200) {
           fatal_error = true;
+        } else {
+          nextCycle();
         }
   });
 }
 
 function checkActiveBattle() {
+  console.log('checkActiveBattle');
   request.get({
       url:SITE_STATUS_URL, 
       jar: true,
@@ -74,14 +91,16 @@ function checkActiveBattle() {
      }, function(err,httpResponse,body) {
     if (err) {
       console.log('error:', err); // Print the error if one occurred
+      nextCycle();
       return;
     }
-    console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
+    console.log('checkActiveBattle', 'statusCode:', httpResponse.statusCode);//, httpResponse.headers);
     //console.log('body:', body); // Print the HTML for the Google homepage.
     battles_in_ready = parseBattles(body, 'Ready in battles');
     battles_in_wait = parseBattles(body, 'Waiting in battles');
+    console.log(battles_in_ready, battles_in_wait);
     for(var i = 0, Ln = battles_in_wait.length; i < Ln; ++i) {
-      forceCloseBattle(battles_in_wait[i]);
+      checkIsBattlePossibleCloseForce(battles_in_wait[i]);
     }
     if (battles_in_ready.length + battles_in_wait.length < CHALLENGE_ACCEPT_CONDITION) {
       checkChallenges();
@@ -91,7 +110,7 @@ function checkActiveBattle() {
   });
 }
 
-function forceCloseBattle(battle_id) {
+function checkIsBattlePossibleCloseForce(battle_id) {
   request.get({
       url:SITE_BATTLE_URL + battle_id,
       jar: true,
@@ -99,28 +118,35 @@ function forceCloseBattle(battle_id) {
      }, function(err,httpResponse,body) {
     if (err) {
       console.log('error:', err); // Print the error if one occurred
+      nextCycle();
       return;
     }
-    console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
+    console.log('checkIsBattlePossibleCloseForce', 'statusCode:', httpResponse.statusCode, httpResponse.headers);
     //console.log('body:', body);
     
     var battle = parseBattle(body);
-    if (battle.force = 1) {
+    battle.battle_id = battle_id;
+    var frm = {
+            force: 1,
+            turn: battle.turn,
+            num: battle.battle_id
+            };
+    console.log(battle_id, battle, JSON.stringify(frm));
+    if (battle.force) {
+      console.log('Try close battle force');
       request.post({
           url:SITE_WARLOCK_SUBMIT, 
           //proxy: 'http://localhost:4128',
           jar: true,
           followRedirect: false,
-          form: {
-            turn: battle.turn,
-            num: battle.battle_id,
-            force: 1
-            }
+          form: frm
          }, function(err,httpResponse,body) {
             if (err) {
               console.log('error:', err); // Print the error if one occurred
+              nextCycle();
               return;
             }
+            console.log('checkIsBattlePossibleCloseForce', 'try close', httpResponse.statusCode, httpResponse.headers);
       });
     }
   });
@@ -128,7 +154,7 @@ function forceCloseBattle(battle_id) {
 }
 
 function parseBattleTurn(body, battle) {
-  battle.turn = getInt(getFieldValue(body, '<H2><U>Turn', ' ', ' '));
+  battle.turn = getInt(getFieldValue(body, '<INPUT TYPE=HIDDEN NAME=turn VALUE=', '"', '"'));
 }
 
 function parseBattleWarlockStatus(data, warlock) {
@@ -187,9 +213,10 @@ function startBattleProcessing(battle_id) {
      }, function(err,httpResponse,body) {
     if (err) {
       console.log('error:', err); // Print the error if one occurred
+      nextCycle();
       return;
     }
-    console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
+    console.log('startBattleProcessing', 'statusCode:', httpResponse.statusCode, httpResponse.headers);
     //console.log('body:', body);
     
     var battle = parseBattle(body);
@@ -292,7 +319,7 @@ function parseChallenges(body) {
   var res = [];
   var idx1 = body.indexOf('<TD CLASS=darkbg>Description</TD>');
   if (idx1 === -1) {
-    return res;
+    return [-1];
   }
   idx1 = body.indexOf('<TR><TD>', idx1);
   var idx2 = body.indexOf('</TABLE>', idx1);
@@ -317,27 +344,40 @@ function parseChallenges(body) {
 //
 
 function checkChallenges() {
+  console.log('checkChallenges');
   request.get({
       url:SITE_CHALLENGES_URL, 
       jar: true,
       followRedirect: true
      }, function(err,httpResponse,body) {
     if (err) {
-      console.log('error:', err); // Print the error if one occurred
+      console.log('error:', httpResponse.statusCode, err); // Print the error if one occurred
+      nextCycle();
       return;
     }
-    console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
+    console.log('checkChallenges', 'statusCode:', httpResponse.statusCode, httpResponse.headers);
+    if (httpResponse.statusCode !== 200) {
+      nextCycle();
+      return;
+    }
     //console.log('body:', body); // Print the HTML for the Google homepage.
-    var battles_to_accept = parseChallenges(body);
+    total_ai_challenges = 0;
     var accepted_cnt = 0;
+    var battles_to_accept = parseChallenges(body);
+    
     for (var i = 0, Ln = battles_to_accept.length; i < Ln; ++i) {
+      if ((i === 0) && (battles_to_accept[i] === -1)) {
+        // something goes wrong, perhapse 
+        nextCycle();
+        return;
+      }
       if (accepted_cnt + battles_in_ready.length + battles_in_wait.length < CHALLENGE_ACCEPT_CONDITION) {
         acceptChallenge(battles_to_accept[i]);
         ++accepted_cnt;
       }
     }
     if ((accepted_cnt + battles_in_ready.length + battles_in_wait.length < CHALLENGE_ADD_CONDITION) && (total_ai_challenges < max_ai_challenges)) {
-      addChallenges(Math.min(CHALLENGE_ADD_CONDITION - (accepted_cnt + battles_in_ready.length + battles_in_wait.length), (max_ai_challenges - total_ai_challenges)));
+      addChallenges(Math.min(CHALLENGE_ADD_CONDITION - (accepted_cnt + battles_in_ready.length + battles_in_wait.length), Math.max(0, max_ai_challenges - total_ai_challenges)));
     } else {
       nextCycle();
     }
@@ -354,21 +394,23 @@ function addChallenges(add_cnt) {
            fast: 1,
            players: 2,
            friendly: 2,
-           game: 1
-           blurb: 'ParaFC Maladroit Training Battle with AI Player, NO BOT allowed ;) join to community https://fb.com/WarlocksDuel/'
+           game: 1,
+           blurb: "ParaFC Maladroit Training Battle with AI Player, NO BOT allowed ;) join to community https://fb.com/WarlocksDuel/"
            }
        }, function(err,httpResponse,body) {
       if (err) {
         console.log('error:', err); // Print the error if one occurred
+        nextCycle();
         return;
       }
-      console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
+      console.log('addChallenges', 'statusCode:', httpResponse.statusCode, httpResponse.headers);
     });
   }
   nextCycle();
 }
 
 function acceptChallenge(battle_id) {
+  console.log('acceptChallenge');
   request.get({
       url:SITE_ACCEPT_CHALLENGE_URL + battle_id, 
       jar: true,
@@ -376,9 +418,10 @@ function acceptChallenge(battle_id) {
      }, function(err,httpResponse,body) {
     if (err) {
       console.log('error:', err); // Print the error if one occurred
+      nextCycle();
       return;
     }
-    console.log('statusCode:', httpResponse.statusCode, httpResponse.headers);
+    console.log('acceptChallenge', 'statusCode:', httpResponse.statusCode, httpResponse.headers);
     //console.log('body:', body);
   });
 }
